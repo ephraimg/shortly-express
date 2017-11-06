f231003a2677827494368fb85c4f403a29d215e5
ephraimg <ephraim.glick@gmail.com>
2017-11-06 12:33:05 -0800

    Pass user creation tests

--- server/app.js
+++ server/app.js
@@ -17,18 +17,15 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/create', 
-(req, res) => {
+app.get('/create', (req, res) => {
   res.render('index');
 });
 
-app.get('/links', 
-(req, res, next) => {
+app.get('/links', (req, res, next) => {
   models.Links.getAll()
     .then(links => {
       res.status(200).send(links);
@@ -38,8 +35,7 @@ app.get('/links',
     });
 });
 
-app.post('/links', 
-(req, res, next) => {
+app.post('/links', (req, res, next) => {
   var url = req.body.url;
   if (!models.Links.isValidUrl(url)) {
     // send back a 404 if link is not valid
@@ -78,7 +74,18 @@ app.post('/links',
 // Write your authentication routes here
 /************************************************************/
 
-
+app.post('/signup', (req, res, next) => {
+  models.Users.get({username: req.body.username})
+    .then(result => {
+      if (result !== undefined) {
+        res.redirect('/signup');
+      } else {
+        let user = req.body;
+        models.Users.create(user)
+          .then(() => res.redirect('/'));
+      }
+    });
+}); 
 
 /************************************************************/
 // Handle the code parameter route last - if all other routes fail
