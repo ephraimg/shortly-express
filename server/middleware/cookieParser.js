d75623cca88bd8cf35b62093eca82bafae17bee6
ephraimg <ephraim.glick@gmail.com>
2017-11-06 16:35:48 -0800

    Complete parseCookies middleware

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  //console.log(req.headers);
+  if (req.headers.cookie) {
+    req.cookies = {};
+    var cookieArray = req.headers.cookie.split('; ').map(cookie => {
+      return cookie.split('=');
+    });
+    cookieArray.forEach(cookie => {
+      var cookieKey = cookie[0];
+      var cookie = cookie[1];
+      req.cookies[cookieKey] = cookie;
+    });
+  }
+  next();
 };
 
 module.exports = parseCookies;
